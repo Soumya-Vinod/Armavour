815cb8ab42114ff46e283abb49917ff7dde8491b
chinmay <chinmaywank12@gmail.com>
2026-07-15 20:28:14 +0530

    fix(infra): shorten migration 0002 revision string to fit varchar(32)

--- infra/migrations/versions/0002_episode_idempotency_and_crash_nullable.py
+++ infra/migrations/versions/0002_episode_idempotency_and_crash_nullable.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from alembic import op
 
-revision = "0002_episode_idempotency_and_crash_nullable"
+revision = "0002_idempot_crash_null"
 down_revision = "0001_create_episodes"
 branch_labels = None
 depends_on = None
